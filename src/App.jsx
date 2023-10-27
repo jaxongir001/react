9688d90c9d3e2f43a115af8ba9aa3525ae7aa4a5
jaxongir001 <jaxongir4990@gmail.com>
2023-10-27 18:45:21 +0500

    werst

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Service from './pages/Service'
+
+function App() {
+  
+
+
+  return (
+    <>
+      <Home/>
+      <Contact/>
+      <About/>
+      <Service/>
+    </>
+  )
+}
+
+export default App
